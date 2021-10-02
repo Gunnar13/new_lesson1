053a1ba28c697208220a947d2d72609469caa08b
Gunnar13 <91275335+Gunnar13@users.noreply.github.com>
2021-10-02 14:45:25 +0300

    Update webpack.config.js
    отредактировал

--- webpack.config.js
+++ webpack.config.js
@@ -36,38 +36,13 @@ module.exports = {                  // 1
                 },
 
             },
-            /*    {
-                    test: /\.css$/i, // 1
-                    use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                name: '[path][contenthash].[ext][query]',
-                            },
-                        },
-                    ],
-                },*/
             {
                 test: /\.css$/,
                 //use: ['style-loader', 'css-loader'], // 2
                 //use: [MiniCssExtractPlugin.loader, 'css-loader'],
                 use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
-            /*    {
-                    test: /\.css$/, // 1
-    
-                    use: ['style-loader', 'css-loader'], // 2
-                    // options: {
-                    //     name: '[path][name].[ext]',
-                    // },
-                },*/
-            /*   {
-                   test: /\.s[ac]ss$/,          // 2
-                   use: ['style-loader', 'css-loader', 'sass-loader'],
-               }*/
-
-
-
+            
         ],
     },
 
@@ -76,72 +51,3 @@ module.exports = {                  // 1
 
 
 
-/*
-
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-
-module.exports = {
-    entry: './js/main.js',
-    output: {
-        path: resolve(__dirname, 'dist'),//'build'
-        //path: 'dist * / ', //resolve(__dirname, 'dist'),//
-        filename: 'main.[contenthash].js' //[contenthash]
-    },
-    plugins: [
-
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css'
-        }),
-        new HtmlWebpackPlugin({
-            template: resolve(__dirname, 'index.html'),
-        }),
-    ],
-
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpe?g|gif)$/i,   //Картинки
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[path][name].[ext][query]',
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(mp3)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[path][name].[ext][query]',
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[path][contenthash].[ext][query]',
-                        },
-                    },
-                ],
-            },
-
-            {
-                test: /\.scss$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-            },
-        ],
-    }
-};
-
-
-*/
